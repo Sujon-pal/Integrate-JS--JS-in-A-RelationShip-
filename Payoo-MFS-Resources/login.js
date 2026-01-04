37e18c52e0616d53c47987a1dd464b3803e8106a
Sujon Pal <dev.sujonpal@gmail.com>
2026-01-04 22:35:49 +0600

    add login page

--- Payoo-MFS-Resources/login.js
+++ Payoo-MFS-Resources/login.js
@@ -1,3 +1,17 @@
-document.getElementById("login-btn").addEventListener("click", function () {
-  console.log("clockend");
-});
+document
+  .getElementById("login-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const acountNumber = document.getElementById("acount-num").value.trim();
+    const pin = document.getElementById("pin").value.trim();
+
+    if (acountNumber.length === 11) {
+      if (pin === "1234") {
+        console.log("yes");
+      } else {
+        console.log("no");
+      }
+    } else {
+      console.log("Enter Valid Acount Number");
+    }
+  });
